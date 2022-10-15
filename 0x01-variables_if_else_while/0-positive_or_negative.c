8d05896ea18e4642a5e82030c4f1a861a8e094bd
ben0backtry <76792678+ben0backtry@users.noreply.github.com>
2022-10-15 03:47:46 -0700

    first commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* local variable definition */
+   int n = 10;
+
+   /* while loop execution */
+   while( n < 20 ) {
+      printf("%d\n", n);
+      n++;
+   }
+ 
+   return 0;
+}
