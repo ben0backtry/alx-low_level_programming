3b531b744a5c9a831d08b175d0ad8fca4b62822f
ben0backtry <76792678+ben0backtry@users.noreply.github.com>
2022-10-16 21:05:41 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,16 +7,16 @@
  */
 int main(void)
 {
-	int i;
-	char a;
-	for (i = 0; i <= 9; i++)
-	{
-		putchar(i + '0');
-	}
-	for (a = 'a'; a <= 'f'; a++)
-	{
-		putchar(a);
-	}
-	putchar('\n');
-	return (0);
+int i;
+char a;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
